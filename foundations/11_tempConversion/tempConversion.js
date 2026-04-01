53c4a23688efe46cad82b23eff5904292be697ed
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-04-01 11:47:50 +0530

    completed tempConversion exercise
    looked for formula and cheated on decimal part

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = ((f-32)*5/9)*10;
+  return Math.round(celsius)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = ((c*9/5)+32)*10;
+  return Math.round(fahrenheit)/10
 };
 
 // Do not edit below this line
